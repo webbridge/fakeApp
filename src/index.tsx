37a3a68ef790b722562c96902b808be5cdacea5e
Andrei Tsapliuk <webbridge@mail.rus>
2019-11-22 11:21:16 +0300

    Added notification for rejecting

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+import { SnackbarProvider } from "notistack";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <SnackbarProvider maxSnack={3}>
+    <App />
+  </SnackbarProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
